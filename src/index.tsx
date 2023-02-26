eba789990375a1a90ed97ed22d9046dadf2a4c76
Nick Dawson <37987430+NicholasDawson@users.noreply.github.com>
2023-02-25 22:41:13 -0500

    combine done 1st pass

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <DndProvider backend={HTML5Backend}>
     <App />
