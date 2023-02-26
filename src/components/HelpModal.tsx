92ffe43fc5edc23c5bab043cc248a57fd32db95c
Devan Kavalchek <devankav7@gmail.com>
2023-02-26 11:27:50 -0500

    Finished help menu

--- src/components/HelpModal.tsx
+++ src/components/HelpModal.tsx
@@ -40,14 +40,30 @@ const buildEq2 = () => {
   return new Equation([five, x4], [y2, nine]);
 };
 
+const buildEq3 = () => {
+  const x = new Variable('x');
+  const y = new Variable('y');
+  const y2 = new Variable('y');
+  const y4 = new Variable('y');
+  const xy = new Element(1, true, Side.Left, [x, y]);
+  const y3 = new Element(3, true, Side.Right, [y2]);
+  const y5 = new Element(2, false, Side.Left, [y4]);
+
+  return new Equation([y5, xy], [y3]);
+};
+
 const samples: Sample[] = [
   {
     text: '6 + 2x = -3 - x',
     equation: buildEq1(),
   },
   {
-    text: '2y - 9 = 4x + 5',
+    text: '-4x - 5 = -2y + 9',
     equation: buildEq2(),
+  },
+  {
+    text: 'xy - 2y = 3y',
+    equation: buildEq3(),
   },
 ];
 
@@ -57,6 +73,16 @@ const HelpModal = ({ modalOpen, onCloseModal, setEquation }: Props) => {
       <div className="modal">
         <div className="content">
           <h2>Help</h2>
+          <ul>
+            <li>Drag items to move them between sides of the equation</li>
+            <li>Drag an item onto another to add them together</li>
+            <li>
+              Tap an item with variables to split them. Drag a variable to
+              divide both sides of the equation by the variable
+            </li>
+            <li>Tap fractions to simplify them</li>
+            <li>Tap the equals sign to flip the sides of the equation</li>
+          </ul>
 
           <h3>Sample Equations</h3>
           <div className="samples">
