b2335ee1d1ac0858e2ce650cc227e0d529981dc4
Devan Kavalchek <devankav7@gmail.com>
2023-02-25 22:06:44 -0500

    Added touch support

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-// import { TouchBackend } from 'react-dnd-touch-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+function isTouchDevice() {
+  return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+}
+
 root.render(
-  <DndProvider backend={HTML5Backend}>
+  <DndProvider backend={isTouchDevice() ? TouchBackend : HTML5Backend}>
     <App />
   </DndProvider>
 );
